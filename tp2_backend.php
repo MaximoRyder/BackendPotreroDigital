3d8b4a56093de45655eeb4bee87783bf186e2290
MaximoRyder <maxigesualdo@gmail.com>
2022-09-14 12:30:13 -0300

    Ejercicios clase 3
    resolucion de ejercicios 1, 2 y 3, pendiente ejercicio 4

---
+++ tp2_backend.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Ejercicio 1: </h1>
+
+    <?php
+    $numero = 13;
+
+    if ($numero > 0) {
+        echo "El numero $numero es un n° positivo";
+    }
+    else {
+        echo "El numero $numero NO es un n° positivo";
+    }
+    ?>
+
+    <br>
+    <hr>
+
+    <h1>Ejercicio 2: </h1>
+
+    <?php
+    if ($numero > 1 && $numero < 10) {
+        echo "El numero $numero es mayor a 1 y menor a 10";
+    }
+    else {
+        echo "El numero $numero no cumple las condiciones";
+    }
+    ?>
+
+    <br>
+    <hr>
+
+    <h1>Ejercicio 3: </h1>
+
+    <?php
+ if ($numero > 10 ) {
+    echo "El numero $numero es mayor a 10";
+}
+elseif ($numero < 2 ) {
+    echo "El numero $numero es menor a 2";
+}
+else {
+    echo "El numero $numero no cumple ninguna de las condiciones";
+}
+    ?>
+
+    <br>
+    <hr>
+
+    <h1>Ejercicio 4: </h1>
+
+    <?php
+
+    ?>
+
+    <br>
+    <hr>
